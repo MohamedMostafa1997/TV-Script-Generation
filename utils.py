b1e2fcb2bf3f3c7b41c5072e7ead9b52f8ccd479
Mohamed Mostafa <thepoet149@gmail.com>
2021-01-25 01:05:58 +0200

    RNN-TV-Script

---
+++ utils.py
@@ -0,0 +1,28 @@
+import re
+from collections import Counter
+
+def preprocess(text):
+
+    # Replace punctuation with tokens so we can use them in our model
+    text = text.lower()
+    text = text.replace('.', ' <PERIOD> ')
+    text = text.replace(',', ' <COMMA> ')
+    text = text.replace('"', ' <QUOTATION_MARK> ')
+    text = text.replace(';', ' <SEMICOLON> ')
+    text = text.replace('!', ' <EXCLAMATION_MARK> ')
+    text = text.replace('?', ' <QUESTION_MARK> ')
+    text = text.replace('(', ' <LEFT_PAREN> ')
+    text = text.replace(')', ' <RIGHT_PAREN> ')
+    text = text.replace('--', ' <HYPHENS> ')
+    text = text.replace('?', ' <QUESTION_MARK> ')
+    # text = text.replace('\n', ' <NEW_LINE> ')
+    text = text.replace(':', ' <COLON> ')
+   
+    
+    # Remove all words with  5 or fewer occurences
+    
+    return text
+
+
+
+
